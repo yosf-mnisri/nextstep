d5d388f04881c475cf51a0159d38a3016cc35d99
yosf-mnisri <youssef.elmnisri24@gmail.com>
2025-08-04 08:32:26 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiWCjQB6EufU3GYthND25RgvowqB37M0U",
-  authDomain: "nextstep-63f12.firebaseapp.com",
-  projectId: "nextstep-63f12",
-  storageBucket: "nextstep-63f12.appspot.com",
-  messagingSenderId: "350910928532",
-  appId: "1:113606248571115335314:web:3131b1a445233d57fdb58669206a7d9ae69b516b"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
